c380ca39333e02abb016d728cedf3bcd23e808b1
Pete Forrest <peter.forrest@abstractleap.com>
2023-12-09 07:17:48 +0000

    Day 9

--- AdventOfCode2023/2023/09/Day9.cs
+++ AdventOfCode2023/2023/09/Day9.cs
@@ -5,22 +5,59 @@ public class Day9 : IDay
     public int Year => 2023;
     public int Day => 9;
 
-    public string? Part1TestSolution => null;
+    public string? Part1TestSolution => "114";
 
     public Task<string> Part1(string input)
     {
         var inputs = input.Split(Environment.NewLine).ToArray();
-        return Task.FromResult("");
+        var nextValue = inputs.Select(l => l.Split(' ').Select(int.Parse).ToArray()).Select(l => SolveForNext(l));
+        return Task.FromResult(nextValue.Sum().ToString());
     }
 
-    public string? Part2TestSolution => null;
+    private int SolveForNext(int[] ints)
+    {
+        var results = new int[ints.Length - 1];
+        for (var i = 0; i < ints.Length - 1; i++)
+        {
+            results[i] = ints[i + 1] - ints[i];
+        }
+
+        if (results.Any(r => r != 0))
+        {
+            return ints[^1] + SolveForNext(results);
+        }
+
+        return ints[^1];
+    }
+
+    public string? Part2TestSolution => "2";
 
 
     public Task<string> Part2(string input)
     {
         var inputs = input.Split(Environment.NewLine).ToArray();
-        return Task.FromResult("");
+        var previousValue = inputs.Select(l => l.Split(' ').Select(int.Parse).ToArray()).Select(l => SolveBack(l));
+        return Task.FromResult(previousValue.Sum().ToString());
+    }
+    
+
+    private int SolveBack(int[] ints)
+    {
+        var results = new int[ints.Length - 1];
+        for (var i = 0; i < ints.Length - 1; i++)
+        {
+            results[i] = ints[i + 1] - ints[i];
+        }
+
+        if (results.Any(r => r != 0))
+        {
+            return ints[0] - SolveBack(results);
+        }
+
+        return ints[0];
     }
 
-    public string? TestInput => null;
+    public string? TestInput => @"0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45";
 }
