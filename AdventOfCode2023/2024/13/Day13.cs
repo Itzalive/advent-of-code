b52273d0afae9a15f5d35983bca9e194c62c5aad
Pete Forrest <pete.forrest@ensek.co.uk>
2024-12-18 22:54:19 +0000

    Day 13 & 14

---
+++ AdventOfCode2023/2024/13/Day13.cs
@@ -0,0 +1,120 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode2023._2024._13;
+
+public class Day13 : IDay
+{
+    public int Year => 2024;
+
+    public int Day => 13;
+
+    public string? Part1TestSolution => "480";
+
+    public Task<string> Part1(string input)
+    {
+        var inputs = input.Split(Environment.NewLine + Environment.NewLine).ToArray();
+        var regex = new Regex(
+            @"Button A\: X\+([0-9]+), Y\+([0-9]+)\r\nButton B\: X\+([0-9]+), Y\+([0-9]+)\r\nPrize\: X\=([0-9]+), Y\=([0-9]+)",
+            RegexOptions.Compiled);
+        var score = 0L;
+        foreach (var clawMachineInput in inputs)
+        {
+            var match = regex.Match(clawMachineInput);
+            var aX = long.Parse(match.Groups[1].Value);
+            var aY = long.Parse(match.Groups[2].Value);
+            var bX = long.Parse(match.Groups[3].Value);
+            var bY = long.Parse(match.Groups[4].Value);
+            var prizeX = long.Parse(match.Groups[5].Value);
+            var prizeY = long.Parse(match.Groups[6].Value);
+
+            var options = new List<long>();
+
+            var aYincrement = (double)prizeY / bY;
+            var ratioY = (double)aY / bY;
+            var a = (prizeX - bX * aYincrement) / (aX - bX * ratioY);
+            var b = aYincrement - ratioY * a;
+            if (Math.Abs(Math.Round(a) - a) < 0.0001 && Math.Abs(Math.Round(b) - b) < 0.0001)
+            {
+                Console.WriteLine($"A: {a} B: {b} Cost:{a * 3 + (long)Math.Round(b)}");
+                options.Add((long)(a * 3 + (long)Math.Round(b)));
+            }
+
+            var newScore = options.Any() ? options.Min() : 0;
+            score += newScore;
+            Console.WriteLine($"Score: {newScore}");
+            Console.WriteLine();
+        }
+
+        return Task.FromResult(score.ToString());
+    }
+
+    public string? Part2TestSolution => null;
+
+
+    public Task<string> Part2(string input)
+    {
+        var inputs = input.Split(Environment.NewLine + Environment.NewLine).ToArray();
+        var regex = new Regex(
+            @"Button A\: X\+([0-9]+), Y\+([0-9]+)\r\nButton B\: X\+([0-9]+), Y\+([0-9]+)\r\nPrize\: X\=([0-9]+), Y\=([0-9]+)",
+            RegexOptions.Compiled);
+        var score = 0L;
+        foreach (var clawMachineInput in inputs)
+        {
+            var match = regex.Match(clawMachineInput);
+            var aX = long.Parse(match.Groups[1].Value);
+            var aY = long.Parse(match.Groups[2].Value);
+            var bX = long.Parse(match.Groups[3].Value);
+            var bY = long.Parse(match.Groups[4].Value);
+            var prizeX = long.Parse(match.Groups[5].Value) + 10000000000000;
+            var prizeY = long.Parse(match.Groups[6].Value) + 10000000000000;
+
+            var options = new List<long>();
+
+            var aYincrement = (decimal)prizeY / bY;
+            var ratioY = (decimal)aY / bY;
+            var a = (prizeX - bX * aYincrement) / (aX - bX * ratioY);
+            var b = aYincrement - ratioY * a;
+            if (Math.Abs(Math.Round(a) - a) < 0.0001m && Math.Abs(Math.Round(b) - b) < 0.0001m)
+            {
+                Console.WriteLine($"A: {a} B: {b} Cost:{a * 3 + (long)Math.Round(b)}");
+                options.Add((long)((long)Math.Round(a) * 3 + (long)Math.Round(b)));
+            }
+
+            var newScore = options.Any() ? options.Min() : 0;
+            score += newScore;
+            Console.WriteLine($"Score: {newScore}");
+            Console.WriteLine();
+        }
+
+        return Task.FromResult(score.ToString());
+    }
+
+    private static ulong GCD(ulong a, ulong b)
+    {
+        while (a != 0 && b != 0)
+        {
+            if (a > b)
+                a %= b;
+            else
+                b %= a;
+        }
+
+        return a | b;
+    }
+
+    public string? TestInput => @"Button A: X+94, Y+34
+Button B: X+22, Y+67
+Prize: X=8400, Y=5400
+
+Button A: X+26, Y+66
+Button B: X+67, Y+21
+Prize: X=12748, Y=12176
+
+Button A: X+17, Y+86
+Button B: X+84, Y+37
+Prize: X=7870, Y=6450
+
+Button A: X+69, Y+23
+Button B: X+27, Y+71
+Prize: X=18641, Y=10279";
+}
---
+++ AdventOfCode2023/2024/14/Day14.cs
@@ -0,0 +1,143 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode2023._2024._14;
+
+public class Day14 : IDay
+{
+    public int Year => 2024;
+
+    public int Day => 14;
+
+    public string? Part1TestSolution => "12";
+
+    public Task<string> Part1(string input)
+    {
+        var inputs = input.Split(Environment.NewLine).ToArray();
+        var bathroomWidth = inputs.Length == 12 ? 11 : 101;
+        var bathroomHeight = inputs.Length == 12 ? 7 : 103;
+        var robots = new List<(int X, int Y, int VelocityX, int VelocityY)>();
+        var regex = new Regex(@"p\=([0-9]+),([0-9]+) v\=(-?[0-9]+),(-?[0-9]+)", RegexOptions.Compiled);
+        foreach (var inputRobot in inputs)
+        {
+            var match = regex.Match(inputRobot);
+            var startX = int.Parse(match.Groups[1].Value);
+            var startY = int.Parse(match.Groups[2].Value);
+            var velocityX = int.Parse(match.Groups[3].Value);
+            var velocityY = int.Parse(match.Groups[4].Value);
+            var endX = (startX + velocityX * 0 + bathroomWidth * 0) % bathroomWidth;
+            var endY = (startY + velocityY * 0 + bathroomHeight * 0) % bathroomHeight;
+            robots.Add((endX, endY, velocityX < 0 ? velocityX + bathroomWidth : velocityX,
+                velocityY < 0 ? velocityY + bathroomHeight : velocityY));
+        }
+
+        for (var i = 1;; i++)
+        {
+            robots = robots.Select(r => ((r.X + r.VelocityX) % bathroomWidth, (r.Y + r.VelocityY) / bathroomHeight,
+                r.VelocityX, r.VelocityY)).ToList();
+
+            Console.WriteLine(i);
+            for (var y = 0; y < bathroomHeight; y++)
+            {
+                for (var x = 0; x < bathroomWidth; x++)
+                {
+                    Console.Write(robots.Count(r => r.X == x && r.Y == y));
+                }
+
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        var quad1 = robots.Count(r => r.X <= (bathroomWidth - 3) / 2 && r.Y <= (bathroomHeight - 3) / 2);
+        var quad2 = robots.Count(r => r.X <= (bathroomWidth - 3) / 2 && r.Y >= (bathroomHeight + 1) / 2);
+        var quad3 = robots.Count(r => r.X >= (bathroomWidth + 1) / 2 && r.Y <= (bathroomHeight - 3) / 2);
+        var quad4 = robots.Count(r => r.X >= (bathroomWidth + 1) / 2 && r.Y >= (bathroomHeight + 1) / 2);
+        Console.WriteLine($"Q1:{quad1} Q2:{quad2} Q3:{quad3} Q4:{quad4}");
+
+        return Task.FromResult((quad1 * quad2 * quad3 * quad4).ToString());
+    }
+
+    public string? Part2TestSolution => null;
+
+
+    public Task<string> Part2(string input)
+    {
+        var inputs = input.Split(Environment.NewLine).ToArray();
+        var bathroomWidth = inputs.Length == 12 ? 11 : 101;
+        var bathroomHeight = inputs.Length == 12 ? 7 : 103;
+        var robots = new List<(int X, int Y, int VelocityX, int VelocityY)>();
+        var regex = new Regex(@"p\=([0-9]+),([0-9]+) v\=(-?[0-9]+),(-?[0-9]+)", RegexOptions.Compiled);
+
+        var initialSeconds = 0;
+        var secondIncrement = 9;
+
+        foreach (var inputRobot in inputs)
+        {
+            var match = regex.Match(inputRobot);
+            var startX = int.Parse(match.Groups[1].Value);
+            var startY = int.Parse(match.Groups[2].Value);
+            var velocityX = int.Parse(match.Groups[3].Value);
+            var velocityY = int.Parse(match.Groups[4].Value);
+            var endX = (startX + velocityX * initialSeconds + bathroomWidth * initialSeconds) % bathroomWidth;
+            var endY = (startY + velocityY * initialSeconds + bathroomHeight * initialSeconds) % bathroomHeight;
+            robots.Add((endX, endY, velocityX < 0 ? velocityX + bathroomWidth : velocityX,
+                velocityY < 0 ? velocityY + bathroomHeight : velocityY));
+        }
+
+        var carryOn = true;
+        var seconds = 0;
+        var maxClosest = 0;
+        for (var i = 1; carryOn; i++)
+        {
+            var progress = 1;//20 + i * 9;
+            robots = robots.Select(r => ((r.X + progress * r.VelocityX) % bathroomWidth, (r.Y + progress * r.VelocityY) % bathroomHeight,
+                r.VelocityX, r.VelocityY)).ToList();
+
+            var closest = robots.Sum(r => robots.Count(r2 => Math.Abs(r.Y - r2.Y) < 2 && Math.Abs(r.X - r2.X) < 2));
+            if (closest > maxClosest)
+            {
+                maxClosest = closest;
+                Console.WriteLine(i);
+                for (var y = 0; y < bathroomHeight; y++)
+                {
+                    for (var x = 0; x < bathroomWidth; x++)
+                    {
+                        var count = robots.Count(r => r.X == x && r.Y == y);
+                        if (count > 0)
+                            Console.BackgroundColor = ConsoleColor.Green;
+                        else
+                            Console.BackgroundColor = ConsoleColor.Black;
+                        Console.Write(count > 0 ? "#" : ".");
+                    }
+
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+        }
+
+        var quad1 = robots.Count(r => r.X <= (bathroomWidth - 3) / 2 && r.Y <= (bathroomHeight - 3) / 2);
+        var quad2 = robots.Count(r => r.X <= (bathroomWidth - 3) / 2 && r.Y >= (bathroomHeight + 1) / 2);
+        var quad3 = robots.Count(r => r.X >= (bathroomWidth + 1) / 2 && r.Y <= (bathroomHeight - 3) / 2);
+        var quad4 = robots.Count(r => r.X >= (bathroomWidth + 1) / 2 && r.Y >= (bathroomHeight + 1) / 2);
+        Console.WriteLine($"Q1:{quad1} Q2:{quad2} Q3:{quad3} Q4:{quad4}");
+
+        return Task.FromResult((quad1 * quad2 * quad3 * quad4).ToString());
+    }
+
+    public string? TestInput => @"p=0,4 v=3,-3
+p=6,3 v=-1,-3
+p=10,3 v=-1,2
+p=2,0 v=2,-1
+p=0,0 v=1,3
+p=3,0 v=-2,-2
+p=7,6 v=-1,-3
+p=3,0 v=-1,-2
+p=9,3 v=2,3
+p=7,3 v=-1,2
+p=2,4 v=2,-3
+p=9,5 v=-3,-3";
+}
