11cf6de82295761f422f8ec05cc1bd2bf5467df0
Pete Forrest <pete.forrest@ensek.co.uk>
2024-12-19 00:31:00 +0000

    Day 15 & 16

---
+++ AdventOfCode2023/2024/15/Day15.cs
@@ -0,0 +1,252 @@
+﻿namespace AdventOfCode2023._2024._15;
+
+public class Day15 : IDay
+{
+    public int Year => 2024;
+
+    public int Day => 15;
+
+    public string? Part1TestSolution => "10092";
+
+    public Task<string> Part1(string input)
+    {
+        var inputs = input.Split(Environment.NewLine + Environment.NewLine).ToArray();
+        var map = inputs[0].Split(Environment.NewLine).Select(l => l.ToCharArray()).ToArray();
+        var commands = inputs[1].Replace("\r\n", "").ToCharArray();
+        var posY = -1;
+        var posX = -1;
+        for (var y = 0; y < map.Length; y++)
+        {
+            for (var x = 0; x < map[y].Length; x++)
+            {
+                if (map[y][x] == '@')
+                {
+                    posY = y;
+                    posX = x;
+                    break;
+                }
+            }
+
+            if (posY != -1)
+                break;
+        }
+
+        var commandDirection = new Dictionary<char, (int X, int Y)>()
+        {
+            { '<', (-1, 0) },
+            { '>', (1, 0) },
+            { '^', (0, -1) },
+            { 'v', (0, 1) },
+        };
+
+        foreach (var command in commands)
+        {
+            var direction = commandDirection[command];
+            var searchX = posX;
+            var searchY = posY;
+            do
+            {
+                searchX += direction.X;
+                searchY += direction.Y;
+                if (map[searchY][searchX] == '#')
+                {
+                    break;
+                }
+
+                if (map[searchY][searchX] == '.')
+                {
+                    map[searchY][searchX] = 'O';
+                    map[posY][posX] = '.';
+                    posY += direction.Y;
+                    posX += direction.X;
+                    map[posY][posX] = '@';
+                    break;
+                }
+            } while (true);
+
+
+        }
+
+        var score = 0;
+        for (var y = 0; y < map.Length; y++)
+        {
+            for (var x = 0; x < map[y].Length; x++)
+            {
+                Console.Write(map[y][x]);
+                if (map[y][x] == 'O')
+                {
+                    score += 100 * y + x;
+                }
+            }
+            Console.WriteLine();
+        }
+
+        return Task.FromResult(score.ToString());
+    }
+
+    public string? Part2TestSolution => null;
+
+
+    public Task<string> Part2(string input)
+    {
+        var inputs = input.Split(Environment.NewLine + Environment.NewLine).ToArray();
+        var map = inputs[0].Split(Environment.NewLine).Select(l => l.ToCharArray()).ToArray();
+        var bigMap = new char[map.Length][];
+        var commands = inputs[1].Replace("\r\n", "").ToCharArray();
+        var posY = -1;
+        var posX = -1;
+        for (var y = 0; y < map.Length; y++)
+        {
+            bigMap[y] = new char[map[y].Length * 2];
+            for (var x = 0; x < map[y].Length; x++)
+            {
+                if (map[y][x] == '#')
+                {
+                    bigMap[y][x * 2] = '#';
+                    bigMap[y][x * 2 + 1] = '#';
+                }
+                else if (map[y][x] == 'O')
+                {
+                    bigMap[y][x * 2] = '[';
+                    bigMap[y][x * 2 + 1] = ']';
+                }
+                else if (map[y][x] == '.')
+                {
+                    bigMap[y][x * 2] = '.';
+                    bigMap[y][x * 2 + 1] = '.';
+                }
+                else if (map[y][x] == '@')
+                {
+                    bigMap[y][x * 2] = '@';
+                    bigMap[y][x * 2 + 1] = '.';
+                    posY = y;
+                    posX = x * 2;
+                }
+            }
+        }
+
+        var commandDirection = new Dictionary<char, (int X, int Y)>()
+        {
+            { '<', (-1, 0) },
+            { '>', (1, 0) },
+            { '^', (0, -1) },
+            { 'v', (0, 1) },
+        };
+
+        foreach (var command in commands)
+        {
+            //foreach (var t in bigMap)
+            //{
+            //    foreach (var t1 in t)
+            //    {
+            //        Console.Write(t1);
+            //    }
+
+            //    Console.WriteLine();
+            //}
+
+            var direction = commandDirection[command];
+            var searchX = posX;
+            var searchY = posY;
+            var boxToMove = new List<(int X, int Y, char value)>();
+            var toCheck = new Queue<(int X, int Y)>();
+            toCheck.Enqueue((posX + direction.X, posY + direction.Y));
+            var canMove = true;
+            while (toCheck.Any())
+            {
+                var positionToCheck = toCheck.Dequeue();
+                if (bigMap[positionToCheck.Y][positionToCheck.X] == ']')
+                {
+                    boxToMove.Add((positionToCheck.X + direction.X - 1, positionToCheck.Y + direction.Y, '['));
+                    boxToMove.Add((positionToCheck.X + direction.X, positionToCheck.Y + direction.Y, ']'));
+                    boxToMove.Add((positionToCheck.X - 1, positionToCheck.Y, '.'));
+                    boxToMove.Add((positionToCheck.X, positionToCheck.Y, '.'));
+
+                    if (direction.X <= 0)
+                    {
+                        toCheck.Enqueue((positionToCheck.X + direction.X - 1, positionToCheck.Y + direction.Y));
+                    }
+
+                    if (direction.X >= 0)
+                    {
+                        toCheck.Enqueue((positionToCheck.X + direction.X, positionToCheck.Y + direction.Y));
+                    }
+                }
+                else if (bigMap[positionToCheck.Y][positionToCheck.X] == '[')
+                {
+                    boxToMove.Add((positionToCheck.X + direction.X, positionToCheck.Y + direction.Y, '['));
+                    boxToMove.Add((positionToCheck.X + direction.X + 1, positionToCheck.Y + direction.Y, ']'));
+                    boxToMove.Add((positionToCheck.X, positionToCheck.Y, '.'));
+                    boxToMove.Add((positionToCheck.X + 1, positionToCheck.Y, '.'));
+
+                    if (direction.X <= 0)
+                    {
+                        toCheck.Enqueue((positionToCheck.X + direction.X, positionToCheck.Y + direction.Y));
+                    }
+
+                    if (direction.X >= 0)
+                    {
+                        toCheck.Enqueue((positionToCheck.X + direction.X + 1, positionToCheck.Y + direction.Y));
+                    }
+                }
+                else if (bigMap[positionToCheck.Y][positionToCheck.X] == '#')
+                {
+                    canMove = false;
+                    break;
+                }
+            }
+
+            if (canMove)
+            {
+                boxToMove.Reverse();
+                foreach (var toMove in boxToMove)
+                {
+                    bigMap[toMove.Y][toMove.X] = toMove.value;
+                }
+
+                bigMap[posY][posX] = '.';
+                posX += direction.X;
+                posY += direction.Y;
+                bigMap[posY][posX] = '@';
+            }
+        }
+
+        var score = 0;
+        for (var y = 0; y < bigMap.Length; y++)
+        {
+            for (var x = 0; x < bigMap[y].Length; x++)
+            {
+                Console.Write(bigMap[y][x]);
+                if (bigMap[y][x] == '[')
+                {
+                    score += 100 * y + x;
+                }
+            }
+            Console.WriteLine();
+        }
+
+        return Task.FromResult(score.ToString());
+    }
+
+    public string? TestInput => @"##########
+#..O..O.O#
+#......O.#
+#.OO..O.O#
+#..O@..O.#
+#O#..O...#
+#O..O..O.#
+#.OO.O.OO#
+#....O...#
+##########
+
+<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^
+vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
+><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<
+<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^
+^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><
+^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^
+>^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^
+<><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>
+^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
+v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^";
+}
---
+++ AdventOfCode2023/2024/16/Day16.cs
@@ -0,0 +1,220 @@
+﻿using System.Collections;
+using AdventOfCode2023._2023._21;
+
+namespace AdventOfCode2023._2024._16;
+
+public class Day16 : IDay
+{
+    public int Year => 2024;
+
+    public int Day => 16;
+
+    public string? Part1TestSolution => "11048";
+
+    public Task<string> Part1(string input)
+    {
+        var map = input.Split(Environment.NewLine).Select(l => l.ToCharArray()).ToArray();
+        var mapScore = Enumerable.Range(0, map.Length)
+            .Select(_ => Enumerable.Range(0, map[0].Length).Select(_ => new[] { int.MaxValue, int.MaxValue, int.MaxValue, int.MaxValue }).ToArray()).ToArray();
+        var posY = -1;
+        var posX = -1;
+        var endY = -1;
+        var endX = -1;
+        for (var y = 0; y < map.Length; y++)
+        {
+            for (var x = 0; x < map[y].Length; x++)
+            {
+                if (map[y][x] == 'S')
+                {
+                    posY = y;
+                    posX = x;
+                }
+
+                if (map[y][x] == 'E')
+                {
+                    endY = y;
+                    endX = x;
+                }
+            }
+        }
+
+        var directions = new[] { (X: 1, Y: 0), (X: 0, Y: -1), (X: -1, Y: 0), (X: 0, Y: 1) };
+        var toCheck = new Queue<(int X, int Y, int Direction, int Score)>();
+        toCheck.Enqueue((posX, posY, 0, 0));
+        while (toCheck.Any())
+        {
+            var checking = toCheck.Dequeue();
+            for (var i = 0; i < 4; i++)
+            {
+                var checkDirection = (checking.Direction + i) % 4;
+                var direction = directions[checkDirection];
+                var newScore = checking.Score + (i == 3 ? 1000: i * 1000) + 1;
+                if (map[checking.Y + direction.Y][checking.X + direction.X] != '#' && mapScore[checking.Y + direction.Y][checking.X + direction.X][checkDirection] > newScore)
+                {
+                    mapScore[checking.Y + direction.Y][checking.X + direction.X][checkDirection] = newScore;
+                    toCheck.Enqueue((checking.X + direction.X, checking.Y + direction.Y, checkDirection, newScore));
+                }
+            }
+        }
+
+        for (var y = 0; y < map.Length; y++)
+        {
+            for (var x = 0; x < map[y].Length; x++)
+            {
+                if (map[y][x] == '#')
+                {
+                    Console.Write('#');
+                }
+                else
+                {
+                    var minScore = mapScore[y][x].Min();
+                    if (minScore == int.MaxValue)
+                    {
+                        Console.Write('.');
+                    }
+                    else
+                    {
+                        if (mapScore[y][x][0] == minScore)
+                        {
+                            Console.Write('>');
+                        }
+                        else if (mapScore[y][x][1] == minScore)
+                        {
+                            Console.Write('^');
+                        }
+                        else if (mapScore[y][x][2] == minScore)
+                        {
+                            Console.Write('<');
+                        }
+                        else if (mapScore[y][x][3] == minScore)
+                        {
+                            Console.Write('v');
+                        }
+                    }
+                }
+            }
+            Console.WriteLine();
+        }
+
+        return Task.FromResult(mapScore[endY][endX].Min().ToString());
+    }
+
+    public string? Part2TestSolution => "64";
+
+
+    public Task<string> Part2(string input)
+    {
+        var map = input.Split(Environment.NewLine).Select(l => l.ToCharArray()).ToArray();
+        var mapScore = Enumerable.Range(0, map.Length)
+            .Select(_ => Enumerable.Range(0, map[0].Length).Select(_ => new[]
+            {
+                (Score: int.MaxValue, Pathway: new List<(int X, int Y)>()),
+                (Score: int.MaxValue, Pathway: new List<(int X, int Y)>()),
+                (Score: int.MaxValue, Pathway: new List<(int X, int Y)>()),
+                (Score: int.MaxValue, Pathway: new List<(int X, int Y)>())
+            }).ToArray()).ToArray();
+        var posY = -1;
+        var posX = -1;
+        var endY = -1;
+        var endX = -1;
+        for (var y = 0; y < map.Length; y++)
+        {
+            for (var x = 0; x < map[y].Length; x++)
+            {
+                if (map[y][x] == 'S')
+                {
+                    posY = y;
+                    posX = x;
+                }
+
+                if (map[y][x] == 'E')
+                {
+                    endY = y;
+                    endX = x;
+                }
+            }
+        }
+
+        var directions = new[] { (X: 1, Y: 0), (X: 0, Y: -1), (X: -1, Y: 0), (X: 0, Y: 1) };
+        var toCheck = new Queue<(int X, int Y, int Direction, int Score, List<(int X, int Y)> Pathway)>();
+        toCheck.Enqueue((posX, posY, 0, 0, []));
+        while (toCheck.Any())
+        {
+            var checking = toCheck.Dequeue();
+            for (var i = 0; i < 4; i++)
+            {
+                var checkDirection = (checking.Direction + i) % 4;
+                var direction = directions[checkDirection];
+                var newScore = checking.Score + (i == 3 ? 1000 : i * 1000) + 1;
+                if (map[checking.Y + direction.Y][checking.X + direction.X] != '#' &&
+                    mapScore[checking.Y + direction.Y][checking.X + direction.X][checkDirection].Score >= newScore)
+                {
+                    if (mapScore[checking.Y + direction.Y][checking.X + direction.X][checkDirection].Score == newScore)
+                    {
+                        mapScore[checking.Y + direction.Y][checking.X + direction.X][checkDirection].Pathway =
+                            [
+                                .. mapScore[checking.Y + direction.Y][checking.X + direction.X][checkDirection].Pathway,
+                                ..checking.Pathway, (checking.X, checking.Y)
+                            ];
+                        mapScore[checking.Y + direction.Y][checking.X + direction.X][checkDirection].Pathway =
+                            mapScore[checking.Y + direction.Y][checking.X + direction.X][checkDirection].Pathway
+                                .Distinct()
+                                .ToList();
+                    }
+                    else
+                    {
+                        mapScore[checking.Y + direction.Y][checking.X + direction.X][checkDirection].Pathway = [..checking.Pathway, (checking.X, checking.Y)];
+                    }
+
+                    mapScore[checking.Y + direction.Y][checking.X + direction.X][checkDirection].Score = newScore;
+                    toCheck.Enqueue((checking.X + direction.X, checking.Y + direction.Y, checkDirection, newScore,
+                        mapScore[checking.Y + direction.Y][checking.X + direction.X][checkDirection].Pathway));
+                }
+            }
+        }
+
+
+        var minScore = mapScore[endY][endX].MinBy(x => x.Score).Score;
+        var bestPathways = mapScore[endY][endX].Where(c => c.Score == minScore).SelectMany(c => c.Pathway).Distinct()
+            .ToArray();
+        for (var y = 0; y < map.Length; y++)
+        {
+            for (var x = 0; x < map[y].Length; x++)
+            {
+                if (map[y][x] == '#')
+                {
+                    Console.Write('#');
+                }
+                else if (bestPathways.Any(p => p.X == x && p.Y == y))
+                {
+                    Console.Write('0');
+                }
+                else
+                {
+                    Console.Write('.');
+                }
+            }
+            Console.WriteLine();
+        }
+
+        return Task.FromResult((bestPathways.Length + 1).ToString());
+    }
+
+    public string? TestInput => @"#################
+#...#...#...#..E#
+#.#.#.#.#.#.#.#.#
+#.#.#.#...#...#.#
+#.#.#.#.###.#.#.#
+#...#.#.#.....#.#
+#.#.#.#.#.#####.#
+#.#...#.#.#.....#
+#.#.#####.#.###.#
+#.#.#.......#...#
+#.#.###.#####.###
+#.#.#...#.....#.#
+#.#.#.#####.###.#
+#.#.#.........#.#
+#.#.#.#########.#
+#S#.............#
+#################";
+}
